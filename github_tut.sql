81ecc7ecb8e7b12b4316ffa923a4b2f72bc7b427
MohdAhad <34982234+MohdAhad@users.noreply.github.com>
2019-12-09 22:15:58 +0530

    Add files via upload

---
+++ github_tut.sql
@@ -0,0 +1,33 @@
+--Git hub tutorial:
+
+
+--Step1: Creating a new repository then adding a readme file in it(can be made public or private).
+
+--Step2: Creating a branch, it help to make changes in another version of original(master) code so that the master code is not changed, it is used to experiment and make
+-- changes before committing them to master, it is basically a different version of same code.
+
+--Steps involved
+*Create branch
+*Commit changes (save changes)
+---Commit mesage is a description of why a particular change was made, Commit messages capture the history of your changes, so other
+-- contributors can understand what you’ve done and why.
+*Submit pull request
+*Merge branch into master
+
+--Step3: Pull Request: Pull requests show diffs, or differences, of the content from both branches. The changes, additions, and subtractions are shown in green and red.
+--By using GitHub’s @mention system in your pull request message, you can ask for feedback from specific people or teams,
+--Steps:
+--Click the  Pull Request tab, then from the Pull Request page, click the green New pull request button.
+--In the Example Comparisons box, select the branch you made, readme-edits, to compare with master (the original).
+--Look over your changes in the diffs on the Compare page, make sure they’re what you want to submit.
+--When you’re satisfied that these are the changes you want to submit, click the big green Create Pull Request button.
+--Give your pull request a title and write a brief description of your changes.
+--Create pull request!
+
+
+--Step4: Merging Pull Request: 
+--Adding the branch to master
+--Steps:
+--Click the green Merge pull request button to merge the changes into master.
+--Click Confirm merge.
+--Go ahead and delete the branch, since its changes have been incorporated, with the Delete branch button in the purple box.
